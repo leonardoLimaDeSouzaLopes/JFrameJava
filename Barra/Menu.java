b7f82c5ca7ac0ffc421a9e51a92d67aa1321db9e
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-10-03 10:41:11 -0300

    Update Menu.java

--- Barra/Menu.java
+++ Barra/Menu.java
@@ -21,6 +21,26 @@ public class Menu extends JFrame{
     	JMenu arquivo = new JMenu("Arquivo");
     	barra.add(arquivo);
     	
+    	//Cria a opcao de novo arquivo
+    	JMenuItem novo = new JMenuItem("Novo");
+    	
+    	arquivo.add(novo);
+    	
+    	//Cria o item Salvar
+    	JMenuItem salvar = new JMenuItem("Salvar");
+    	
+    	arquivo.add(salvar);
+    	
+    	//Cria o item Salvar Como
+    	JMenuItem salvarComo = new JMenuItem("Salvar Como");
+    	
+    	arquivo.add(salvarComo);
+    	
+    	//Cria o Item Carregar
+    	JMenuItem carregar = new JMenuItem("Carregar");
+    	
+    	arquivo.add(carregar);
+    	
     	// Cria o item Sair
     	JMenuItem sair = new JMenuItem("Sair");
     	sair.addActionListener(new ActionListener() {
@@ -29,6 +49,42 @@ public class Menu extends JFrame{
     		}
     	});
     	arquivo.add(sair);
+    	
+    	//Cria o menu cadastrar
+    	JMenu cadastrar = new JMenu("Cadastrar");
+    	barra.add(cadastrar);   	
+    	
+    	//Cria o item categoria
+    	JMenuItem categoria = new JMenuItem("Categoria");
+    	
+    	cadastrar.add(categoria);
+    	
+    	//Cria o item Produto
+    	JMenuItem produto = new JMenuItem("Produto");
+    	
+    	cadastrar.add(produto);
+    	
+    	//Cria o item cliente
+    	JMenuItem cliente = new JMenuItem("Cliente");
+    	
+    	cadastrar.add(cliente);
+    	
+    	//Cria o item pedidos
+    	JMenuItem pedido = new JMenuItem("Pedido");
+    	
+    	cadastrar.add(pedido);
+    	
+
+    	//Cria o menu ajuda
+    	JMenu ajuda = new JMenu("Ajuda");
+    	barra.add(ajuda);   	
+    	
+    	//Cria o item categoria
+    	JMenuItem sobre = new JMenuItem("Sobre");
+    	
+    	ajuda.add(sobre);
+
+    	
     	
        
     	this.setVisible(true);
