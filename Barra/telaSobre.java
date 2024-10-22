c4a63326762c25016fbc5cd4584ec82051fb9814
Triaguin <magalhaes.dev147@gmail.com>
2024-10-22 19:56:06 -0300

    Update telaSobre.java

--- Barra/telaSobre.java
+++ Barra/telaSobre.java
@@ -1,10 +1,32 @@
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+
 import javax.swing.*;
-public class telaSobre extends JFrame{
+public class telaSobre extends JFrame {
+	
+	private JButton btsobre;
+	
 	public telaSobre() {		
-		this.setTitle("Sobre");
+		this.setTitle("Cadastrar Produto");
 	    this.setSize(900,600);  
 	    this.setResizable(false);
-	    this.setLocationRelativeTo(null);
+	    this.setLocationRelativeTo(null);		
+	    setLayout(null);
+	    
+	    btsobre = new JButton();
+	    btsobre.setText("Ver sobre a gente ");
+	    btsobre.setBounds(350,200,200,100);
+	    add(btsobre);
+	    
+	    btsobre.addActionListener(new ActionListener() {
+	    	
+	    	public void actionPerformed(ActionEvent e) {
+	    		JOptionPane.showMessageDialog(null, "lol");
+	    	}
+	    	
+	    });
+	    
 	}
 }
