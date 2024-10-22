ba8300306ab9c05ff58e88999e2adac5f402e604
Triaguin <magalhaes.dev147@gmail.com>
2024-10-22 19:36:37 -0300

    Arrumei a pagína pedido

--- Barra/CadastrarPedido.java
+++ Barra/CadastrarPedido.java
@@ -1,15 +1,25 @@
-
 import javax.swing.*;
 
 public class CadastrarPedido extends JFrame {
-	private JLabel lbnomePedido;
-	private JTextField txnomePedido;
-	private JButton btCadastrarPedido;
+	private JButton btnovoPedido;
+	private JButton btverPedido;
 	
 	public CadastrarPedido() {		
 		this.setTitle("Cadastrar Categoria");
 	    this.setSize(900,600);  
 	    this.setResizable(false);
 	    this.setLocationRelativeTo(null);
+	    setLayout(null); // Importante
+	    
+	    btnovoPedido = new JButton();
+	    btnovoPedido.setText("Novo Pedido");
+	    btnovoPedido.setBounds(250,20,400,100);
+	    add(btnovoPedido);
+	    
+	    btverPedido = new JButton();
+	    btverPedido.setText("Ver Pedidos");
+	    btverPedido.setBounds(250,200,400,100);	    
+	    add(btverPedido);
 	}
+	
 }
