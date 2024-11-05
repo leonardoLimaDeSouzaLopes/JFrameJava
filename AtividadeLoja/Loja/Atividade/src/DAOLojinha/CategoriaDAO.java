66b2b64de3451984bf4e05898a6103da988551f5
Triaguin <magalhaes.dev147@gmail.com>
2024-11-05 20:34:22 -0300

    05-11-24

--- AtividadeLoja/Loja/Atividade/src/DAOLojinha/CategoriaDAO.java
+++ AtividadeLoja/Loja/Atividade/src/DAOLojinha/CategoriaDAO.java
@@ -13,14 +13,14 @@ public class CategoriaDAO {
 		this.connection = new ConnectionFactory().getConnection(); // Cada vez que criar uma instancia, ira conectar com o banco de dados
 	}
 	
-	public void adicionarCategoria(ModelarCategoria nomeCategoria) throws SQLException {
+	public void adicionarCategoria(ModelarCategoria Categoria) throws SQLException {
 		try {
 			
 			String tbCategoria = "insert into tbCategoria (nomeCategoria) values (?)";
 			
 			PreparedStatement stmt = connection.prepareStatement (tbCategoria); //preparando parar executar o codigo
 			
-			stmt.setString(1, nomeCategoria.getNomeCategoria());
+			stmt.setString(1, Categoria.getNomeCategoria());
 			
 			stmt.execute();
 			stmt.close();
